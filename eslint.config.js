533e35722494a56dabe03c5d9a7452e144f9c515
egy186 <egy186@gmail.com>
2024-09-17 21:44:00 +0900

    chore(test): use node:assert and node:test (#192)

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,2 @@
-import { base } from '@egy186/eslint-config';
-// eslint-disable-next-line import/no-unresolved
-import { jest } from '@egy186/eslint-config/jest';
-
-const config = [
-  { ignores: ['coverage'] },
-  base,
-  {
-    ...jest,
-    files: ['**/*.test.js']
-  },
-  {
-    files: ['eslint.config.js'],
-    rules: {
-      'n/no-unpublished-import': 'off'
-    }
-  }
-];
-
-export default config;
+// eslint-disable-next-line n/no-unpublished-import
+export { default } from '@egy186/eslint-config';
